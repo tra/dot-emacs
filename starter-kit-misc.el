2e73b3956f70ca990844d0b1c54df438f23a1392
Tom Anderson <tom@squeat.com>
2010-06-12 22:33:49 -0600

    don't ignore .less files and open them in css-mode

--- starter-kit-misc.el
+++ starter-kit-misc.el
@@ -93,9 +93,8 @@
 
 (add-to-list 'auto-mode-alist '("COMMIT_EDITMSG$" . diff-mode))
 (add-to-list 'auto-mode-alist '("\\.css$" . css-mode))
+(add-to-list 'auto-mode-alist '("\\.less$" . css-mode))
 (add-to-list 'auto-mode-alist '("\\.ya?ml$" . yaml-mode))
-(add-to-list 'auto-mode-alist '("\\.rb$" . ruby-mode))
-(add-to-list 'auto-mode-alist '("Rakefile$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("\\.js\\(on\\)?$" . javascript-mode))
 (add-to-list 'auto-mode-alist '("\\.xml$" . nxml-mode))
 
