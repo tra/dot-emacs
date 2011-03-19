eedcb1a54941eed517452c9cb2fba9bae9f171c6
Tom Anderson <tanderson@comverge.com>
2011-03-18 20:22:54 -0600

    backspace while searching

--- starter-kit-bindings.el
+++ starter-kit-bindings.el
@@ -96,6 +96,7 @@
   (lambda () (interactive)
     (let ((case-fold-search isearch-case-fold-search))
       (occur (if isearch-regexp isearch-string (regexp-quote isearch-string))))))
+(define-key isearch-mode-map [backspace] 'isearch-delete-char)
 
 (provide 'starter-kit-bindings)
 ;;; starter-kit-bindings.el ends here
